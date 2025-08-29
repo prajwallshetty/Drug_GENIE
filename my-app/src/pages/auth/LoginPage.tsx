36a64bd7f1ee373b165d43e8ac0459080c7c1de2
pajju011 <prajwal.r.poojary11@gmail.com>
2025-08-29 21:13:31 +0530

    dd

--- my-app/src/pages/auth/LoginPage.tsx
+++ my-app/src/pages/auth/LoginPage.tsx
@@ -144,7 +144,7 @@ const LoginPage: React.FC = () => {
                 </div>
               </motion.div>  
 
-              <motion.div
+              <motion.div   
                 initial={{ opacity: 0, y: 20 }} 
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.3 }}
